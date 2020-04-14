b9b44139be51e0ac90aef9e15db8b853fad9b124
liupeng <liupeng.0@outlook.com>
2020-04-14 23:31:35 +0800

    Test V 1.0.1

---
+++ src/components/login/Login.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <div class="outer_label">
+            <img class="inner_label login_logo" src="../../assets/logo.png">
+        </div>
+        <div class="login_form">
+            <input type="text"  class="qxs-ic_user qxs-icon"  placeholder="用户名" v-model="userName">
+            <input type="password"  class="qxs-ic_password qxs-icon"  placeholder="密码" v-model="password">
+            <el-button class="login_btn" @click.native="login" type="primary" round :loading="isBtnLoading">登录</el-button>
+            <div>
+                <el-button type="primary" @click="login" class="login_style">登录</el-button>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+    //  import { userLogin } from '../../api/api';
+
+    export default {
+        data() {
+            return {
+                userName: '',
+                password: '',
+                isBtnLoading: false
+            }
+        },
+        created () {
+            if(JSON.parse( localStorage.getItem('user')) && JSON.parse( localStorage.getItem('user')).userName){
+                this.userName = JSON.parse( localStorage.getItem('user')).userName;
+                this.password = JSON.parse( localStorage.getItem('user')).password;
+            }
+        },
+        computed: {
+            btnText() {
+                if (this.isBtnLoading) return '登录中...';
+                return '登录';
+            }
+        },
+        methods: {
+            login() {
+                if (!this.userName) {
+                    this.$message.error('请输入用户名');
+                    return;
+                }
+                if (!this.password) {
+                    this.$message.error('请输入密码');
+                    return;
+                }
+
+            }
+        }
+    }
+</script>
+<style>
+    .login_form {
+        padding-top: 10%;
+        padding-left: 10%;
+        padding-right: 10%;
+    }
+    .qxs-ic_user {
+        background: url("../../assets/icons/User/user-3-fill.svg") no-repeat;
+        background-size: 13px 15px;
+        background-position: 3%;
+    }
+    .qxs-ic_password {
+        background: url("../../assets/icons/System/lock-password-line.svg") no-repeat;
+        background-size: 13px 15px;
+        background-position: 3%;
+        margin-bottom: 20px;
+    }
+    .login_logo {
+        height: 100%;
+    }
+    .login_btn {
+        width: 100%;
+        font-size: 16px;
+        background: -webkit-linear-gradient(left, #19cbc5, #fa216d); /* Safari 5.1 - 6.0 */
+        background: -o-linear-gradient(right, #e24cce, #6e0edc); /* Opera 11.1 - 12.0 */
+        background: -moz-linear-gradient(right, #bf7a46, #2154FA); /* Firefox 3.6 - 15 */
+        background: linear-gradient(to right, #4dac52, #a71a2f); /* 标准的语法 */
+        filter: brightness(1.4);
+    }
+    .login_style{
+        width: 200px;
+    }
+</style>
